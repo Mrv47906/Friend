d9835c2baad5e348a864bd3041165e93551efd57
MR.VIRUS <60812262+Mrv47906@users.noreply.github.com>
2020-02-28 17:40:19 +0530

    Add files via upload

---
+++ Friend.sh
@@ -0,0 +1,75 @@
+#coded by mrv47906
+#if you use it then give me cradit
+
+#colours
+cyan='\033[1;36m'
+green='\033[1;32m'
+red='\033[1;31m'
+yellow='\033[1;33m'
+b='\033[1;34m'
+p='\033[1;35m'
+reset='\033[0m'
+
+echo -e "\e[1;31m     ______     _                __
+   / ____/____(_)__  ____  ____/ /
+  / /_  / ___/ / _ \/ __ \/ __  / 
+ / __/ / /  / /  __/ / / / /_/ /  
+/_/   /_/  /_/\___/_/ /_/\__,_/   
+                                    \e[0m"
+
+echo -e "\e[1;31m {MRV47906} \e[0m"
+
+echo  $p 'Please Wait ===+['$b'./                   '$p']\'
+sleep 0.4
+echo  $p 'Please Wait ===+['$b'..\                  '$p']|'
+sleep 0.4
+echo  $p 'Please Wait ===+['$b'.../                 '$p']/'
+sleep 0.4
+echo  $p 'Please Wait ===+['$b'....\                '$p']\'
+sleep 0.4
+echo  $p 'Please Wait ===+['$b'...../               '$p']|'
+sleep 0.4
+echo  $p 'Please Wait ===+['$b'......./             '$p']/'
+sleep 0.5
+echo  $p 'Please Wait ===+['$b'........\            '$p']|'
+sleep 0.5
+echo  $p 'Please Wait ===+['$b'........../          '$p']\'
+sleep 0.5
+echo  $p 'Please Wait ===+['$b'..........\          '$p']/'
+sleep 0.5
+echo  $p 'Please Wait ===+['$b'............./       '$p']|'
+sleep 0.6
+echo  $p 'Please Wait ===+['$b'..............\      '$p']/'
+sleep 0.6
+echo  $p 'Please Wait ===+['$b'............./       '$p']\'
+sleep 0.6
+echo  $p 'Please Wait ===+['$b'...............\     '$p']|'
+sleep 0.5
+echo  $p 'Please Wait ===+['$b'................/    '$p']/'
+sleep 0.4
+echo  $p 'Please Wait ===+['$b'.................\   '$p']\'
+sleep 0.4
+echo  $p 'Please Wait ===+['$b'.................../ '$p']|'
+sleep 0.4
+
+NOTE :> NOW THE INSTOLATION IS STARTED
+clear
+
+apt-get update -y
+
+pkg install fish -y
+
+pkg install wget -y
+echo -e "\e[1;31m     ____  ____  _   ________
+   / __ \/ __ \/ | / / ____/
+  / / / / / / /  |/ / __/   
+ / /_/ / /_/ / /|  / /___   
+/_____/\____/_/ |_/_____/   
+                             \e[0m"
+
+
+echo -e "\e[1;42m A SCRIPT BY MR.VIRUS\e[0m"
+
+
+echo -e "\e[1;45m Enter <fish> to start the tool every time \e[0m"
+
